f115f7ebffbe707d4dea52390743944f783ed569
mery b <mbratkowska@poczta.onet.pl>
2016-12-05 23:28:50 +0100

    initial version

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// plik scripts.js
+
+var a = prompt("Jeśli chcesz policzyć pole trójkąta, podaj długość jego podstawy - a");
+var h = prompt("Wskaż też jego wysokość - h");
+ 
+var triangleArea = a*h/2;
+ 
+alert("Pole Twojego trójkąta wynosi " + triangleArea);
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
